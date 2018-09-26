b8c9505ab71f650043579ea5849c9407708ab088
logolevel <logolevel@gmail.com>
2018-09-27 02:33:52 +0300

    add js animation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 function newOne() {
-    alert('2 seconds');
+
+    var welcomeText = $(".first__welcome-text");
+    var generalContent = $(".first__general");
+
+    welcomeText.animate({"opacity": "1" }, 1500 ).delay(2000);
+    welcomeText.animate({"opacity": "0" }, 1500, function() {
+        welcomeText.css({"display": "none"});
+    });
+
+
+
+
+
 }
 
-setTimeout(newOne, 2000);
+setTimeout(newOne, 1000);
