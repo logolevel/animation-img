7e81318b9366d87f3c38c1e5511834404b52eff7
logolevel <logolevel@gmail.com>
2018-09-28 01:51:30 +0300

    normal version

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 function newOne() {
 
-    var welcomeText = $(".first__welcome-text");
-    var firstView = $(".first-view-enabled");
-    var firstGeneral = $('.first__general');
+    var welcomeText = $(".first__welcome-text"),
+        firstView = $(".first-view-enabled"),
+        firstGeneral = $('.first__general'),
+        headerBlock = $('.header');
 
-    welcomeText.animate({"opacity": "1" }, 1500 ).delay(2000);
-    welcomeText.animate({"opacity": "0" }, 1500, function() {
-        welcomeText.css({"display": "none"});
-    });
-
-    function secondStep() {
-        firstGeneral.css({"display" : "block"}).animate({"opacity": "1" }, 1500, function() {
+    welcomeText.animate({opacity: "1"}, {duration: 1500}).delay(2000);
+    welcomeText.animate({opacity: "0"}, {duration: 1500, complete:function() {
+        welcomeText.css({"display": "none"}).delay(100);
+        firstGeneral.css({"display" : "block"}).animate({opacity: "1"}, {duration: 1500, complete:function() {
             firstView.removeClass('first-view-enabled');
-        });
-    }
-
-
-    setTimeout(secondStep, 4000);
-
-
-
+            headerBlock.animate({opacity: "1"}, {duration: 500});
+        }})
+        
+    }});
+    
 }
 
 setTimeout(newOne, 1000);
